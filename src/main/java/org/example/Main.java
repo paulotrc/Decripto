b482cdccbc9310380402cd4359e110f2a66990f9
Paulo Carneiro <prolimca@emeal.nttdata.com>
2024-09-04 11:44:24 -0300

    Example to runs Has and base64 methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Example Using Hash Function
+
+
+        //Example Using Base64 Codification
         System.out.println("Hello world!");
     }
 }
